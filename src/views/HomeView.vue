365303e33bdf99e746a9be9060059b96ca739cfe
GarlicBuns19 <planterx26@gmail.com>
2022-10-19 16:29:40 +0200

    data

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,11 +1,24 @@
 <template>
   <div class="home">
     <h1>Home</h1>
+    <div v-for="project in projects" :key="project.id">
+      {{project.id}}
+    </div>
   </div>
 </template>
 
 <script>
 export default {
   components: {},
+  data(){
+    return {
+      projects : []
+    }
+  },
+  mounted(){
+    fetch('http://localhost:3000/projects')
+    .then(res => res.json())
+    .then(data => this.projects = data)
+  }
 };
 </script>
